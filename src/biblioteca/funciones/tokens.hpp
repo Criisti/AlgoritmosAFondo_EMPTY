e0fa0a7393a97dbec74c6bf5ef1a0e349059736a
Criisti <cristianchicomaedu@gmail.com>
2023-05-13 00:45:23 -0300

    v2_8vo commit todo funciones

--- src/biblioteca/funciones/tokens.hpp
+++ src/biblioteca/funciones/tokens.hpp
@@ -6,7 +6,7 @@
 #include "strings.hpp"
 using namespace std;
 
-int tokenCount(string s,char sep)
+int tokenCount(string& s,char sep)
 {
     int cantSep= charCount(s,sep);
     int i=1, posSep, posSep2=0;
@@ -36,26 +36,68 @@ int tokenCount(string s,char sep)
 
 }
 
-void addToken(string& s,char sep,string t)
-{
+void addToken(string& s,char sep,string t) {
+    s+=sep+t;
 }
 
 string getTokenAt(string s,char sep, int i)
 {
-   return "";
+    int cantTokens= tokenCount(s,sep);
+    int posSep, posSep2=0;
+    string cadToken;
+    while(cantTokens>=0){
+        if (cantTokens==i){
+            posSep= indexOfN(s,sep,i+1);
+            if (i==0){
+                cadToken= substring(s,posSep2,posSep);
+            }else{
+                posSep2= indexOfN(s,sep,i);
+                cadToken= substring(s,posSep2+1,posSep);
+            }
+        }
+        cantTokens--;
+    }
+    return cadToken;
 }
 
-void removeTokenAt(string& s,char sep, int i)
-{
+void removeTokenAt(string& s,char sep, int i) {
+
+    if(i==0) {
+        int posTokenEli = indexOfN(s, sep, i+1);//+1 para buscar el primero sep
+        s= substring(s,posTokenEli+1);// +1 elimina el separador del inicio
+    }else{
+        int posTokenEli = indexOfN(s, sep, i);
+        string sIni = substring(s, 0, posTokenEli);
+
+        string TokenEli = getTokenAt(s, sep, i);
+        int l = length(TokenEli);
+        string sFin = substring(s, posTokenEli + l+1);// +1 para quedarse con el sep del token final
+        s = sIni + sFin;
+    }
 }
 
 void setTokenAt(string& s,char sep, string t,int i)
 {
+   if(i==0){
+       string ss=s;
+       s="";
+       s=t+sep+ss;
+   }else {
+       int posToken= indexOfN(s,sep,i);
+       string sA= substring(s,0,posToken);
+       string sB= substring(s,posToken);
+       s=sA+sep+t+sB;
+   }
 }
 
 int findToken(string s,char sep, string t)
 {
-   return 0;
+   int posTokens= tokenCount(s,sep);
+   while(posTokens>=0){
+       if(getTokenAt(s,sep,posTokens)==t)return posTokens;
+       posTokens--;
+   }
+   if(posTokens==-1)return -1;
 }
 
 #endif
